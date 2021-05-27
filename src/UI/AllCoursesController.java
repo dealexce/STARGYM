c0316c0ba304084cdbde4bd776cf61943937b433
dealexce <dealexce@sina.com>
2021-05-27 13:26:18 +0800

    Iteration 4 Front-end

--- src/UI/AllCoursesController.java
+++ src/UI/AllCoursesController.java
@@ -3,6 +3,7 @@ package UI;
 import Data.Course;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -59,16 +60,35 @@ public class AllCoursesController extends ManagedPage{
             gp2.setMinWidth(240);
             Label lbl2 = new Label(course.getCourseId());
             lbl2.setPrefSize(180,15);
-            Button btn2 = new Button("Start");
+            Button btn2 = new Button("Like");
             btn2.setMinWidth(60);
+            btn2.setOnMouseClicked(e->like(course));
+            Button btn3 = new Button("Start");
+            btn3.setMinWidth(60);
             gp2.add(lbl2,0,0);
             gp2.add(btn2,1,0);
+            gp2.add(btn3,2,0);
 
             vb.getChildren().addAll(lbl,gp2);
             gp.add(vb,0,1);
 
             coursePane.getChildren().add(gp);
 
+        }
+    }
+
+    private void like(Course course){
+        if(this.stageManager.getDataService().getTrainee()!=null){
+            this.stageManager.getDataService().traineeAddCourse(course.getCourseId());
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Success");
+            alert.setContentText("You like this courses! You can see this course in your personal page then.");
+            alert.showAndWait();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Fail to like this course");
+            alert.setContentText("Only members can like a course. Please login as a member first.");
+            alert.showAndWait();
         }
     }
 }
--- src/UI/AllTrainersController.java
+++ src/UI/AllTrainersController.java
@@ -1,14 +1,86 @@
 package UI;
 
+import Data.Course;
+import Data.Trainer;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.control.*;
+
+import java.util.List;
 
 /**
  * @description: Controller of AllTrainersPage
  * @author: Haopu Chen
  **/
 public class AllTrainersController extends ManagedPage{
+    @FXML
+    private VBox trainerBox;
     public static final String path = Path.ALLTRAINERS;
     public void goHome(ActionEvent actionEvent) {
         this.stageManager.stageRedirect(path,Path.HOME);
     }
+
+    @Override
+    public void init() {
+        genTrainerBox();
+    }
+
+    private void genTrainerBox(){
+        List<Trainer> trainers = this.stageManager.getDataService().getAllTrainer();
+        for(Trainer trainer:trainers){
+            BorderPane bp = new BorderPane();
+            bp.setPrefSize(600,100);
+            bp.setMinSize(600,100);
+            bp.setStyle("-fx-border-color: lightgrey; -fx-background-color: FCFCFC;");
+            String url = System.getProperty("user.dir")+ "\\res\\Trainers\\T1.png";
+            javafx.scene.image.Image img = new Image("file:"+url);
+            ImageView imgv = new ImageView(img);
+            imgv.setFitHeight(80);
+            imgv.setFitWidth(80);
+            imgv.setPreserveRatio(false);
+            imgv.setPickOnBounds(true);
+            bp.setLeft(imgv);
+            BorderPane.setAlignment(imgv, Pos.CENTER);
+
+            VBox vb = new VBox();
+            vb.setPrefSize(100,200);
+            Label lbl = new Label(trainer.getUserName());
+            lbl.setFont(Font.font(23));
+            Line line = new Line();
+            line.setStartX(-100);
+            line.setEndX(100);
+            Label lbl2 = new Label("Level "+ trainer.getLevel());
+            lbl2.setTextFill(Color.BLUE);
+            Label lbl3 = new Label(trainer.getIntroduction());
+            vb.getChildren().addAll(lbl,lbl2,lbl3);
+            BorderPane.setMargin(vb,new Insets(10));
+            BorderPane.setAlignment(vb, Pos.CENTER);
+            bp.setCenter(vb);
+
+            VBox vb2 = new VBox();
+            vb2.setPrefSize(100,200);
+            Button btn = new Button("Book");
+            Button btn2 = new Button("Like");
+            vb2.getChildren().addAll(btn,btn2);
+            BorderPane.setMargin(vb2,new Insets(15,10,5,15));
+            bp.setRight(vb2);
+
+
+
+            trainerBox.getChildren().add(bp);
+
+        }
+    }
+
 }
